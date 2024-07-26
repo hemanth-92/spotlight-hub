8012d1c498d72bb807b80fdc9bc3ab9e7bb79f6d
Raphael-08 <mohanvarma130@gmail.com>
2024-07-26 22:24:38 +0530

    change

--- src/app/components/DropDown-menu.tsx
+++ src/app/components/DropDown-menu.tsx
@@ -6,27 +6,15 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { AlignJustify } from "lucide-react";
 export default function Dropdown() {
   return (
     <div className="border-white bg-[#A3A3A3] p-0 text-white">
       <DropdownMenu>
         <DropdownMenuTrigger className="px-2 text-black">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-7"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-            />
-          </svg>
+        <AlignJustify />
         </DropdownMenuTrigger>
-        <DropdownMenuContent side="top" align="end" sideOffset={20}>
+        <DropdownMenuContent side="top" align="end" sideOffset={15}>
           <DropdownMenuLabel>DashBoard</DropdownMenuLabel>
           <DropdownMenuSeparator />
           <DropdownMenuItem>My Account</DropdownMenuItem>
