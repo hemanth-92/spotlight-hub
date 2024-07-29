0f4cab3330cece729d416951ae2f8af856794ab8
hemanth-92 <131354764+hemanth-92@users.noreply.github.com>
2024-07-29 22:54:15 +0530

    fix:icons state

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,7 +1,17 @@
+"use client";
+
 import { cn } from "@/lib/utils";
 import { type ClassValue } from "clsx";
 import Image from "next/image";
-import { EllipsisVertical, Copy, Link } from "lucide-react";
+import {
+  EllipsisVertical,
+  Copy,
+  Link,
+  Check,
+  Edit,
+  Trash2,
+} from "lucide-react";
+import { useState } from "react";
 
 interface ImageCardProps {
   ImageUrl: string;
@@ -9,6 +19,22 @@ interface ImageCardProps {
 }
 
 export default function ImageCard({ ImageUrl, className }: ImageCardProps) {
+  const [icon1, setIcon1] = useState("link");
+  const [icon2, setIcon2] = useState("copy");
+  const [icon3, setIcon3] = useState("ellipsis");
+
+  const toggleIcon1 = () => {
+    setIcon1((prevIcon) => (prevIcon === "link" ? "check" : "link"));
+  };
+
+  const toggleIcon2 = () => {
+    setIcon2((prevIcon) => (prevIcon === "copy" ? "edit" : "copy"));
+  };
+
+  const toggleIcon3 = () => {
+    setIcon3((prevIcon) => (prevIcon === "ellipsis" ? "trash" : "ellipsis"));
+  };
+
   return (
     <div
       className={cn("flex flex-col rounded-xl bg-accent shadow-lg", className)}
@@ -24,12 +50,47 @@ export default function ImageCard({ ImageUrl, className }: ImageCardProps) {
         <div className="absolute right-2 top-2 z-10 flex space-x-2">
           <button className="rounded-md bg-white/0 p-2 shadow-md backdrop-blur-md">
             <Link className="h-4 w-4 text-icon-foreground hover:text-icon" />
+        <Image
+          src={ImageUrl}
+          alt="logo"
+          width={300}
+          height={300}
+          className="h-52 rounded-t-xl object-cover"
+        />
+        <div className="absolute right-2 top-2 z-10 flex space-x-2">
+          <button
+            onClick={toggleIcon1}
+            className="rounded-md bg-white/0 p-2 shadow-md backdrop-blur-md"
+          >
+            {icon1 === "link" ? (
+              <Link className="h-4 w-4 text-icon-foreground transition-transform hover:text-icon dark:rotate-0 dark:scale-100" />
+            ) : (
+              <Edit className="h-4 w-4 text-icon-foreground transition-transform hover:text-icon dark:rotate-0 dark:scale-100" />
+            )}
           </button>
           <button className="rounded-md bg-white/0 p-2 shadow-md backdrop-blur-md">
             <Copy className="h-4 w-4 text-icon-foreground hover:text-icon" />
+          <button
+            onClick={toggleIcon2}
+            className="rounded-md bg-white/0 p-2 shadow-md backdrop-blur-md"
+          >
+            {icon2 === "copy" ? (
+              <Copy className="h-4 w-4 text-icon-foreground hover:text-icon" />
+            ) : (
+              <Check className="h-4 w-4 text-icon-foreground hover:text-icon" />
+            )}
           </button>
           <button className="rounded-md bg-white/0 p-2 shadow-md backdrop-blur-md">
             <EllipsisVertical className="h-4 w-4 text-icon-foreground hover:text-icon" />
+          <button
+            onClick={toggleIcon3}
+            className="rounded-md bg-white/0 p-2 shadow-md backdrop-blur-md"
+          >
+            {icon3 === "ellipsis" ? (
+              <EllipsisVertical className="h-4 w-4 text-icon-foreground hover:text-icon" />
+            ) : (
+              <Trash2 className="h-4 w-4 text-icon-foreground hover:text-icon" />
+            )}
           </button>
         </div>
       </div>
