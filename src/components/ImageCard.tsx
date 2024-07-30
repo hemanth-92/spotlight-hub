6bc4bf2c71fce75186c2cd26c487a5cbefd5ef21
hemanth-92 <131354764+hemanth-92@users.noreply.github.com>
2024-07-30 22:43:36 +0530

    add menubar

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -3,15 +3,13 @@
 import { cn } from "@/lib/utils";
 import { type ClassValue } from "clsx";
 import Image from "next/image";
-import {
-  EllipsisVertical,
-  Copy,
-  Link,
-  Check,
-  Edit,
-  Trash2,
-} from "lucide-react";
+import { EllipsisVertical, Copy, Link, Check, Edit } from "lucide-react";
 import { useState } from "react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
 interface ImageCardProps {
   ImageUrl: string;
@@ -21,7 +19,11 @@ interface ImageCardProps {
 export default function ImageCard({ ImageUrl, className }: ImageCardProps) {
   const [icon1, setIcon1] = useState("link");
   const [icon2, setIcon2] = useState("copy");
-  const [icon3, setIcon3] = useState("ellipsis");
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+
+  const handlePopoverToggle = () => {
+    setIsPopoverOpen(!isPopoverOpen);
+  };
 
   const toggleIcon1 = () => {
     setIcon1((prevIcon) => (prevIcon === "link" ? "check" : "link"));
@@ -37,12 +39,6 @@ export default function ImageCard({ ImageUrl, className }: ImageCardProps) {
     }, 1000);
   };
 
-  const toggleIcon3 = () => {
-    setIcon3((prevIcon) => (prevIcon === "ellipsis" ? "trash" : "ellipsis"));
-    setTimeout(() => {
-      setIcon3("ellipsis");
-    }, 1000);
-  };
   return (
     <div
       className={cn("flex flex-col rounded-xl bg-accent shadow-lg", className)}
@@ -56,62 +52,66 @@ export default function ImageCard({ ImageUrl, className }: ImageCardProps) {
           className="h-52 rounded-t-xl object-cover"
         />
         <div className="absolute right-2 top-2 z-10 flex space-x-2">
-          <Image
-            src={ImageUrl}
-            alt="logo"
-            width={300}
-            height={300}
-            className="h-52 rounded-t-xl object-cover"
-          />
-          <div className="absolute right-2 top-2 z-10 flex space-x-2">
-            <button
-              onClick={toggleIcon1}
-              className="rounded-md bg-white/0 p-2 shadow-md backdrop-blur-md"
-            >
-              {icon1 === "link" ? (
-                <Link className="h-4 w-4 text-icon-foreground transition-all hover:text-icon dark:rotate-0 dark:scale-100" />
-              ) : (
-                <Edit className="h-4 w-4 text-icon-foreground transition-all hover:text-icon dark:rotate-0 dark:scale-100" />
-              )}
-            </button>
-            <button
-              onClick={toggleIcon2}
-              className="rounded-md bg-white/0 p-2 shadow-md backdrop-blur-md"
-            >
-              {icon2 === "copy" ? (
-                <Copy className="h-4 w-4 text-icon-foreground transition-all hover:text-icon" />
-              ) : (
-                <Check className="h-4 w-4 text-icon-foreground transition-all hover:text-icon" />
-              )}
-            </button>
-            <button
-              onClick={toggleIcon3}
-              className="rounded-md bg-white/0 p-2 shadow-md backdrop-blur-md"
-            >
-              {icon3 === "ellipsis" ? (
+          <button
+            onClick={toggleIcon1}
+            className="rounded-md bg-white/0 p-2 shadow-md backdrop-blur-md"
+          >
+            {icon1 === "link" ? (
+              <Link className="h-4 w-4 text-icon-foreground transition-all hover:text-icon" />
+            ) : (
+              <Edit className="h-4 w-4 text-icon-foreground transition-all hover:text-icon" />
+            )}
+          </button>
+          <button
+            onClick={toggleIcon2}
+            className="rounded-md bg-white/0 p-2 shadow-md backdrop-blur-md"
+          >
+            {icon2 === "copy" ? (
+              <Copy className="h-4 w-4 text-icon-foreground transition-all hover:text-icon" />
+            ) : (
+              <Check className="h-4 w-4 text-icon-foreground transition-all hover:text-icon" />
+            )}
+          </button>
+          <Popover>
+            <PopoverTrigger asChild>
+              <button
+                onClick={handlePopoverToggle}
+                aria-expanded={isPopoverOpen}
+                aria-label="More options"
+                className="rounded-md bg-white/0 p-2 shadow-md backdrop-blur-md"
+              >
                 <EllipsisVertical className="h-4 w-4 text-icon-foreground transition-all hover:text-icon" />
-              ) : (
-                <Trash2 className="h-4 w-4 text-icon-foreground transition-all hover:text-icon" />
-              )}
-            </button>
+              </button>
+            </PopoverTrigger>
+            <PopoverContent
+              align="end"
+              className="w-full bg-white/10 p-3 shadow-md backdrop-blur-md"
+            >
+              <div className="flex flex-col">
+                <button className="text-left text-lg">Rename</button>
+                <button className="text-left text-lg">Resize</button>
+                <button className="text-left text-lg">Edit</button>
+                <button className="text-left text-lg">Delete</button>
+              </div>
+            </PopoverContent>
+          </Popover>
+        </div>
+      </div>
+      <div className="flex flex-col p-2">
+        <div className="flex w-full justify-between p-2">
+          <div className="text-2xl font-bold text-foreground">Title</div>
+          <div className="rounded-full bg-png px-4 py-1 font-bold text-png-foreground">
+            .png
           </div>
         </div>
-        <div className="flex flex-col p-2">
-          <div className="flex w-full justify-between p-2">
-            <div className="text-2xl font-bold text-foreground">Title</div>
-            <div className="bg-png text-png-foreground rounded-full px-4 py-1 font-bold">
-              .png
+        <div className="mt-4 flex w-full justify-between p-2">
+          <div className="flex w-7/12 gap-1">
+            <div className="text-muted-foreground">alt</div>
+            <div className="flex items-center pt-0.5 text-sm text-foreground">
+              This is some alt
             </div>
           </div>
-          <div className="mt-4 flex w-full justify-between p-2">
-            <div className="flex w-7/12 gap-1">
-              <div className="text-muted-foreground">alt</div>
-              <div className="flex items-center pt-0.5 text-sm text-foreground">
-                This is some alt
-              </div>
-            </div>
-            <div className="test-sm text-png flex items-center">600 x 600</div>
-          </div>
+          <div className="flex items-center text-sm text-png">600 x 600</div>
         </div>
       </div>
     </div>
