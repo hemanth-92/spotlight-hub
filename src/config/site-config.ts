0947cb4c4650aa2cbb88f6dcf1a12619ae1bf797
hemanth-92 <131354764+hemanth-92@users.noreply.github.com>
2024-08-04 14:53:27 +0530

    landing page

---
+++ src/config/site-config.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  name: "Spotlight Hub",
+  url: "https://spotlight-hub.vercel.app/",
+  description:
+    " Spotlight Hub is a comprehensive image management solution that leverages GitHub for storage, APIs for dynamic image handling, and AI for image enhancement and smart cropping. It integrates with jsDelivr CDN for efficient image serving and includes features to simplify the user experience in uploading and managing images within their projects.",
+  links: {
+    docs: "/docs/introduction",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
