d1eb1c886099b443ed1fd16dcd19ab951965577e
Raphael-08 <mohanvarma130@gmail.com>
2024-08-07 11:47:03 +0530

    fix: better way to get username

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -2,19 +2,24 @@ import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
 import { JWT } from "next-auth/jwt";
 
 declare module "next-auth" {
+  interface Profile extends Profile {
+    login: string;
+  }
+
   interface User extends DefaultUser {
     oauthToken?: string;
-    username :string;
+    username?: string;
   }
 
   interface Session extends DefaultSession {
     oauthToken?: string;
-    user : User
+    user: User;
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
     oauthToken?: string;
+    username?: string;
   }
 }
